db61e13ace1abfb79716276a9e33073dde442bb4
bcbeltran <benjamin.beltran37@gmail.com>
2022-01-29 14:40:48 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Coin from "./components/Coin";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img
+					src="https://cryptologos.cc/logos/xrp-xrp-logo.png"
+					className="App-logo"
+					alt="logo"
+				/>
+				<h1 className="headingH1">Coin Exhcange</h1>
+			</header>
+      <bold><hr /></bold>
+			<table>
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Ticker</th>
+						<th>Price</th>
+					</tr>
+				</thead>
+				<tbody>
+					<Coin name="Bitcoin" ticker="BTC" price={38000} />
+					<Coin name="XRP" ticker="XRP" price={.72} />
+					<Coin name="USDC" ticker="USDC" price={1} />
+					<Coin name="Cardano" ticker="ADA" price={1.5} />
+				</tbody>
+			</table>
+		</div>
+	);
 }
 
 export default App;
